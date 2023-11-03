6abb95d80d8689f65db532d2673f645797440497
Prajwal A K <118243416+Prajwal-ak-0@users.noreply.github.com>
2023-11-03 11:19:24 +0530

    Added Home Page, Icon

---
+++ first/thirdViewController.swift
@@ -0,0 +1,42 @@
+//
+//  thirdViewController.swift
+//  first
+//
+//  Created by Apple on 03/11/23.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+   @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        button.setTitle("\u{1F9EE}", for: .normal)
+//        
+//        button.setTitleColor(.black, for: .normal)
+//        button.titleLabel?.font = UIFont.systemFont(ofSize: 100.0)
+//        
+//        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    @objc func buttonTapped(){
+//        print("Tapped!")
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
